975ecea632fe57177be1a86d8593a0c6a602a4c0
Samuel Micallef <sammy.m1285@gmail.com>
2019-11-28 21:13:16 +1100

    Initial workflow setup

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,10 +2,6 @@
 var Clock;
 var i = 1;
 
-var menuCanvas = document.getElementById("theMenuCanvas");
-menuCanvas.width = window.innerWidth;
-menuCanvas.height = window.innerHeight;
-
 function Start(){
     Clock = setInterval(Animation, 500);
 }
@@ -16,10 +12,7 @@ function Animation(){
         i++;
     }else if(i == 1){
         document.getElementById("underScore").style.color = "#86C232";
-        i--;s
+        i--;
     }
 }
 
-function reloadPage(){
-    window.open("file:///C:/Users/sammy/Desktop/total%20website/root1/index.html", "_self")
-}
